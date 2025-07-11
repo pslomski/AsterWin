bae7ea2bf34d41c8cacb2c0d92dcba2c1e4ad083
Piotr Słomski <pslomski@tlen.pl>
2025-07-11 22:58:05 +0200

    Add TestScoreCounter

---
+++ src/game/ut/TestScoreCounter.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include "game/ScoreCounter.hpp"
+
+using namespace testing;
+
+namespace ge
+{
+class TestScoreCounter : public Test
+{
+protected:
+    ScoreCounter::OnIncrementLives onIncrementLives;
+    ScoreCounter counter{onIncrementLives};
+};
+
+TEST_F(TestScoreCounter, shouldCreate)
+{
+    ASSERT_EQ(0, counter.get());
+}
+
+TEST_F(TestScoreCounter, shouldInc)
+{
+    counter.inc(2);
+    counter.inc(3);
+    ASSERT_EQ(5, counter.get());
+}
+
+TEST_F(TestScoreCounter, shouldReset)
+{
+    counter.inc(10);
+    counter.reset();
+    ASSERT_EQ(0, counter.get());
+}
+} // namespace ge
