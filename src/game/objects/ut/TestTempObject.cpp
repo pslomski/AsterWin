88f648a933a90b8cafe569cc5ce2880074dc39e3
Piotr Słomski <pslomski@tlen.pl>
2025-07-15 09:37:58 +0200

    Add TestTempObject

---
+++ src/game/objects/ut/TestTempObject.cpp
@@ -0,0 +1,20 @@
+#include "game/objects/TempObject.cpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace testing;
+
+namespace game
+{
+class TestTempObject : public Test
+{
+protected:
+    TempObject object;
+};
+
+TEST_F(TestTempObject, testExpired)
+{
+    ASSERT_FALSE(object.expired());
+}
+} // namespace game
