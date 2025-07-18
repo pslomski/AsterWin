9d96d179bfe9eb52ca3c0cb3308dc4a1e89c3b0a
Piotr Słomski <pslomski@tlen.pl>
2025-07-18 08:48:43 +0200

    Extract SampleId

--- src/audio/SfxSamples.hpp
+++ src/audio/SfxSamples.hpp
@@ -3,27 +3,11 @@
 #include <bass.h>
 #include <cstdint>
 #include <map>
-#include "audio/Consts.hpp"
+#include "audio/SampleId.hpp"
 
 namespace audio
 {
 using Flags = uint32_t;
-
-enum class SampleId
-{
-    shipEngine = SND_SHIP_ENGINE,
-    shipFire = SND_SHIP_FIRE,
-    shipCrash = SND_SHIP_CRASH,
-    asterCrash1 = SND_ASTER_CRASH1,
-    asterCrash2 = SND_ASTER_CRASH2,
-    asterCrash3 = SND_ASTER_CRASH3,
-    ufoEngine = SND_UFO_ENGINE,
-    startBeep = SND_START_BEEP,
-    bonusBeep = SND_BONUS_BEEP,
-    broom = SND_BROOM,
-    powerUp = SND_POWERUP,
-    shipFirePower = SND_SHIP_FIRE_POWER
-};
 
 class SfxSamples
 {
