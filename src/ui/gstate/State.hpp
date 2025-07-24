9ae64d643eebd30c6dffac9d470481fd912ad946
Piotr Słomski <pslomski@tlen.pl>
2025-07-24 23:14:44 +0200

    Add [[maybe_unused]] attributes in State

--- src/ui/gstate/State.hpp
+++ src/ui/gstate/State.hpp
@@ -15,8 +15,8 @@ public:
     virtual void onKeyDown(WPARAM) {}
     virtual void onKeyUp(WPARAM) {}
     virtual void onChar(WPARAM) {}
-    virtual void onResize(int cx, int cy) {}
-    virtual void update(double timeStep) {}
+    virtual void onResize([[maybe_unused]] const int cx, [[maybe_unused]] const int cy) {}
+    virtual void update([[maybe_unused]] const double timeStep) {}
     virtual void draw() {}
     virtual void enterState() {}
     virtual void leaveState() {}
